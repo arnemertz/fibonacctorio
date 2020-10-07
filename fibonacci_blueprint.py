1554408762b3bf9476f00c22d2cf6878c1cea864
Arne Mertz <github@arne-mertz.de>
2020-10-07 07:39:37 +0200

    refactor

--- fibonacci_blueprint.py
+++ fibonacci_blueprint.py
@@ -5,9 +5,6 @@ import math
 true_tile="hazard-concrete-right"
 false_tile="concrete"
 
-def icon(index,digit):
-    return {"signal": {"type": "virtual", "name": "signal-%d" % digit}, "index": index+1}
-
 def inDistance(x, y, radius):
     return x*x + y*y <= radius*radius
 
@@ -23,6 +20,9 @@ def fatfilled( x, y, radius ):
         inDistance(x - 1, y + 1, radius)
     )
 
+def filled(x, y, radius):
+    return fatfilled(x, y, radius) and not(fatfilled(x + 1, y, radius) and fatfilled(x, y + 1, radius))
+
 def tiles(radius):
     tiles = []
     width_r = float(radius)
@@ -32,28 +32,32 @@ def tiles(radius):
     while y <= maxblocks_x / 2 - 1:
         x = 0.5
         while x <= maxblocks_x / 2 - 1:
-            filled = fatfilled(x, y, radius) and not(fatfilled(x + 1, y, radius) and fatfilled(x, y + 1, radius))
-            if filled:
-                tiles.append({"position": {"x": int(x-0.5), "y": int(y-0.5)}, "name": true_tile if filled else false_tile})
+            if filled(x, y, radius):
+                tiles.append({"position": {"x": int(x-0.5), "y": int(y-0.5)}, "name": true_tile})
             x += 1
         y += 1
     return tiles
 
+def icon(index,digit):
+    return {"signal": {"type": "virtual", "name": "signal-%d" % digit}, "index": index+1}
+
+def icons(radius):
+    assert(radius <= 9999)
+    digits=[int(d) for d in str(radius)]
+    return [ icon(index, digit) for index,digit in enumerate(digits) ]
 
 def fib(N):
     return fib(N-1) + fib(N-2) if N>2 else 1
 
 def blueprint(N):
     radius = fib(N)
-    assert(radius <= 9999)
 
-    json_object={"blueprint":{"icons":[],"tiles":[],"item":"blueprint","version":281474976710656},"version_byte":"0"}
+    json_object={"blueprint":{"icons":[],"tiles":[],"item":"blueprint","version":281474976710656}}
     blueprint = json_object["blueprint"]
 
-    digits=[int(d) for d in str(radius)]
-    blueprint["icons"] = [ icon(index, digit) for index,digit in enumerate(digits) ]
-
+    blueprint["icons"] = icons(radius)
     blueprint["tiles"] = tiles(radius)
+    blueprint["label"] = "fib(%d)" % N
 
     return Blueprint(json_object)
 
@@ -66,3 +70,4 @@ for N in range(6,7):
 
 book.replace_indexes()
 book.to_exchange_file('output/blueprintbook.txt')
+book.to_json_file('output/blueprintbook.json')
