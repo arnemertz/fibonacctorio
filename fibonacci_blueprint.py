53c1380a170888ed2afb93cc7db3546cd5eb13a6
Arne Mertz <github@arne-mertz.de>
2020-11-03 07:44:24 +0100

    filled and partially filled quarter circles

--- fibonacci_blueprint.py
+++ fibonacci_blueprint.py
@@ -2,8 +2,8 @@ from factorio.blueprints import *
 import json
 import math
 
-true_tile="hazard-concrete-right"
-false_tile="concrete"
+border_tile="hazard-concrete-right"
+off_tile="concrete"
 
 def inDistance(x, y, radius):
     return x*x + y*y <= radius*radius
@@ -23,7 +23,7 @@ def fatfilled( x, y, radius ):
 def filled(x, y, radius):
     return fatfilled(x, y, radius) and not(fatfilled(x + 1, y, radius) and fatfilled(x, y + 1, radius))
 
-def tiles(radius):
+def tiles(radius, full=False, all=False):
     tiles = []
     width_r = float(radius)
     maxblocks_x = math.ceil(width_r - .5) * 2 + 1
@@ -33,9 +33,9 @@ def tiles(radius):
         x = 0.5
         while x <= maxblocks_x / 2 - 1:
             if filled(x, y, radius):
-                tiles.append({"position": {"x": int(x-0.5), "y": int(y-0.5)}, "name": true_tile})
-            elif filled(x+1, y, radius) or filled(x+2, y, radius) or filled(x, y+1, radius) or filled(x, y+2, radius):
-                tiles.append({"position": {"x": int(x-0.5), "y": int(y-0.5)}, "name": false_tile})
+                tiles.append({"position": {"x": int(x-0.5), "y": int(y-0.5)}, "name": border_tile})
+            elif all or (full and inDistance(x, y, radius)) or filled(x+1, y, radius) or filled(x+2, y, radius) or filled(x, y+1, radius) or filled(x, y+2, radius):
+                tiles.append({"position": {"x": int(x-0.5), "y": int(y-0.5)}, "name": off_tile})
             x += 1
         y += 1
     return tiles
@@ -51,24 +51,28 @@ def icons(radius):
 def fib(N):
     return fib(N-1) + fib(N-2) if N>2 else 1
 
-def blueprint(N):
+def blueprint(N, full=False, all=False):
     radius = fib(N)
 
     json_object={"blueprint":{"icons":[],"tiles":[],"item":"blueprint","version":281474976710656}}
     blueprint = json_object["blueprint"]
 
     blueprint["icons"] = icons(radius)
-    blueprint["tiles"] = tiles(radius)
-    blueprint["label"] = "fib(%d)" % N
+    blueprint["tiles"] = tiles(radius, full, all)
+    blueprint["label"] = "fib(%d) - %s" % (N, "all" if all else "full" if full else "border")
 
     return Blueprint(json_object)
 
 
+blueprint_range = range(6,15)
 
 book=BlueprintBook.from_exchange_file('init_blueprintbook.txt')
-for N in range(6,7):
-    bp = blueprint(N)
-    book.add_blueprint(bp)
+for N in blueprint_range:
+    book.add_blueprint(blueprint(N))
+for N in blueprint_range:
+    book.add_blueprint(blueprint(N, full=True))
+for N in blueprint_range:
+    book.add_blueprint(blueprint(N, all=True))
 
 book.replace_indexes()
 book.to_exchange_file('output/blueprintbook.txt')
